5fc204b643e754a310175d1d2dd338b9ca6d99ea
Guoqi <maguoqi96@gmail.com>
2020-02-08 13:07:00 -0500

    Show event information

--- app/src/main/java/com/laioffer/matrix/MainFragment.java
+++ app/src/main/java/com/laioffer/matrix/MainFragment.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
@@ -20,6 +21,8 @@ import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,6 +39,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -57,7 +61,7 @@ import static android.app.Activity.RESULT_OK;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MainFragment extends Fragment implements OnMapReadyCallback, ReportDialog.DialogCallBack {
+public class MainFragment extends Fragment implements OnMapReadyCallback, ReportDialog.DialogCallBack,GoogleMap.OnMarkerClickListener {
     private static final int REQUEST_CAPTURE_IMAGE = 100;
     private final String path = Environment.getExternalStorageDirectory() + "/temp.png";
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
@@ -91,6 +95,17 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
     private FloatingActionButton fabFocus;
     private DatabaseReference database;
 
+    //event information part
+    private BottomSheetBehavior bottomSheetBehavior;
+    private ImageView mEventImageLike;
+    private ImageView mEventImageComment;
+    private ImageView mEventImageType;
+    private TextView mEventTextLike;
+    private TextView mEventTextType;
+    private TextView mEventTextLocation;
+    private TextView mEventTextTime;
+    private TrafficEvent mEvent;
+
 
     //Add instance to allow ControlPanel to fetch the MainFragment instance.
 
@@ -116,6 +131,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReference();
         verifyStoragePermissions(getActivity());
+        setupBottomBehavior();
         return view;
     }
 
@@ -153,6 +169,7 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
     @Override
     public void onMapReady(GoogleMap googleMap) {
         MapsInitializer.initialize(getContext());
+        googleMap.setOnMarkerClickListener(this);
         this.googleMap = googleMap;
         this.googleMap.setMapStyle(
                 MapStyleOptions.loadRawResourceStyle(
@@ -378,6 +395,68 @@ public class MainFragment extends Fragment implements OnMapReadyCallback, Report
                 //TODO: do something
             }
         });
+    }
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        mEvent = (TrafficEvent) marker.getTag();
+        if (mEvent == null) {
+            return false;
+        }
+        String user = mEvent.getEvent_reporter_id();
+        String type = mEvent.getEvent_type();
+        long time = mEvent.getEvent_timestamp();
+        double latitude = mEvent.getEvent_latitude();
+        double longitutde = mEvent.getEvent_longitude();
+        int likeNumber = mEvent.getEvent_like_number();
+
+        String description = mEvent.getEvent_description();
+        marker.setTitle(description);
+        mEventTextLike.setText(String.valueOf(likeNumber));
+        mEventTextType.setText(type);
+
+        mEventImageType.setImageDrawable(ContextCompat.getDrawable(getContext(), Config.trafficMap.get(type)));
+
+        if (user == null) {
+            user = "";
+        }
+        String info = "Reported by " + user + " " + Utils.timeTransformer(time);
+        mEventTextTime.setText(info);
+
+
+        int distance = 0;
+        locationTracker = new LocationTracker(getActivity());
+        locationTracker.getLocation();
+        if (locationTracker != null) {
+            distance = Utils.distanceBetweenTwoLocations(latitude, longitutde, locationTracker.getLatitude(), locationTracker.getLongitude());
+        }
+        mEventTextLocation.setText(distance + " miles away");
+
+        if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        } else {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        }
+        return true;
+    }
+
+    private void setupBottomBehavior() {
+        //set up bottom up slide
+        final View nestedScrollView = (View) view.findViewById(R.id.nestedScrollView);
+        bottomSheetBehavior = BottomSheetBehavior.from(nestedScrollView);
+
+        //set hidden initially
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        //set expansion speed
+        bottomSheetBehavior.setPeekHeight(1000);
+
+        mEventImageLike = (ImageView) view.findViewById(R.id.event_info_like_img);
+        mEventImageComment = (ImageView) view.findViewById(R.id.event_info_comment_img);
+        mEventImageType = (ImageView) view.findViewById(R.id.event_info_type_img);
+        mEventTextLike = (TextView) view.findViewById(R.id.event_info_like_text);
+        mEventTextType = (TextView) view.findViewById(R.id.event_info_type_text);
+        mEventTextLocation = (TextView) view.findViewById(R.id.event_info_location_text);
+        mEventTextTime = (TextView) view.findViewById(R.id.event_info_time_text);
     }
 
 }
--- app/src/main/java/com/laioffer/matrix/Utils.java
+++ app/src/main/java/com/laioffer/matrix/Utils.java
@@ -9,6 +9,7 @@ import org.apache.commons.codec.binary.Hex;
 
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Util class stores util static method
@@ -67,6 +68,24 @@ public class Utils {
                 bm, 0, 0, width, height, matrix, false);
         bm.recycle();
         return resizedBitmap;
+    }
+    public static String timeTransformer(long millis) {
+        long currenttime = System.currentTimeMillis();
+        long diff = currenttime - millis;
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(diff);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(diff);
+        long hours = TimeUnit.MILLISECONDS.toHours(diff);
+        long days = TimeUnit.MILLISECONDS.toDays(diff);
+
+        if (seconds < 60) {
+            return seconds + " seconds ago";
+        } else if (minutes < 60) {
+            return minutes + " minutes ago";
+        } else if (hours < 24) {
+            return hours + " hours ago";
+        } else {
+            return days + " days ago";
+        }
     }
 
 }
